27ecc85a14f21a2446abcf89fcc1f1422dace8ba
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2021-03-29 18:33:57 +0200

    Improve entrypoint

--- laravel-echo/bin/docker-entrypoint.sh
+++ laravel-echo/bin/docker-entrypoint.sh
@@ -3,15 +3,18 @@ set -e
 
 # laravel-echo-server init
 if [[ "$1" == 'init' ]]; then
+    echo "Set config"
     set -- laravel-echo-server "$@"
 fi
 
 if [ "$1" == 'start' ] && [ -f '/app/laravel-echo-server.lock' ]; then
+    echo "Removing config"
     rm /app/laravel-echo-server.lock
 fi
 
 # laravel-echo-server <sub-command>
 if [[ "$1" == 'start' ]] || [[ "$1" == 'client:add' ]] || [[ "$1" == 'client:remove' ]]; then
+    echo "Managing config"
     if [[ "${LARAVEL_ECHO_SERVER_GENERATE_CONFIG:-true}" == "false" ]]; then
         # wait for another process to inject the config
         echo -n "Waiting for /app/laravel-echo-server.json"
@@ -22,11 +25,12 @@ if [[ "$1" == 'start' ]] || [[ "$1" == 'client:add' ]] || [[ "$1" == 'client:rem
     elif [[ ! -f /app/laravel-echo-server.json ]]; then
         cp /usr/local/src/laravel-echo-server.json /app/laravel-echo-server.json
         # Replace with environment variables
+        echo -n "Replace with environment variables"
         sed -i "s|LARAVEL_ECHO_SERVER_DB|${LARAVEL_ECHO_SERVER_DB:-redis}|i" /app/laravel-echo-server.json
         sed -i "s|REDIS_HOST|${REDIS_HOST:-redis}|i" /app/laravel-echo-server.json
         sed -i "s|REDIS_PORT|${REDIS_PORT:-6379}|i" /app/laravel-echo-server.json
         sed -i "s|REDIS_PASSWORD|${REDIS_PASSWORD}|i" /app/laravel-echo-server.json
-        sed -i "s|REDIS_PREFIX|${REDIS_PREFIX:-laravel_database_}|i" /app/laravel-echo-server.json
+        sed -i "s|REDIS_PREFIX|${REDIS_PREFIX:-echo_srv_}|i" /app/laravel-echo-server.json
         sed -i "s|REDIS_DB|${REDIS_DB:-0}|i" /app/laravel-echo-server.json
         # Remove password config if it is empty
         sed -i "s|\"password\": \"\",||i" /app/laravel-echo-server.json
@@ -36,6 +40,7 @@ fi
 
 # first arg is `-f` or `--some-option`
 if [[ "${1#-}" != "$1" ]]; then
+    echo "Set config"
 	set -- laravel-echo-server "$@"
 fi
 
